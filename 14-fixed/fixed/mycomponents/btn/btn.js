d9a382f49f72e63706de05c7f13dad6c301bb5e0
biao <1252635765@qq.com>
2021-01-06 17:10:27 +0800

    fixed

--- 14-fixed/fixed/mycomponents/btn/btn.js
+++ 14-fixed/fixed/mycomponents/btn/btn.js
@@ -8,10 +8,20 @@ Component({
     formType: {
       type: String,
       value: ""
+    },
+    url: {
+      type: String,
+      value: ""
     }
   },
   methods: {
     tap(){
+      let url = this.data.url;
+      if(url){
+        wx.navigateTo({
+          url,
+        });
+      }
       this.triggerEvent("getTap")
     }
   }
---
+++ 14-fixed/fixed/pages/orderlist/orderlist.js
@@ -0,0 +1,92 @@
+// pages/orderlist/orderlist.js
+let that;
+let util = require("../../utils/util");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    that = this;
+
+    // init
+    this.init();
+  },
+
+  // init
+  init(){
+
+    // calculate height
+    this.calculateHeight();
+
+  },
+
+  // calculate height
+  calculateHeight(){
+    let sysHeight = wx.getSystemInfoSync().windowHeight;
+    let query = wx.createSelectorQuery();
+    query.select(".tab-box").boundingClientRect();
+    query.exec(res=>{
+      let contHeight = sysHeight - res[0].height;
+      that.setData({
+        contHeight
+      });
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
