01cfdf862f099b364296de5595694bce45e5d1a8
biao <1252635765@qq.com>
2021-01-06 21:37:16 +0800

    fixed

---
+++ 14-fixed/fixed/mycomponents/status/status.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    icon: {
+      type: String,
+      value: ""
+    },
+    text: {
+      type: String,
+      value: ""
+    },
+    selected: {
+      type: Boolean,
+      value: false
+    }
+  }
+});
